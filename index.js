cbe4e29cc024d53548ec10ccd415c8b7c8b87221
sohan-99 <sohan75632@gmail.com>
2023-11-20 01:14:36 +0600

    Create a JWT token and save it on local storage

--- index.js
+++ index.js
@@ -35,9 +35,34 @@ async function run() {
  const cartcollection = client.db("bistroDB").collection("carts");
 
 
-// user related api
+  // jwt related api
+  app.post('/jwt', async (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+    res.send({ token });
+  })
 
-app.get('/users', async(req, res)=>{
+
+   // middlewares 
+   const verifyToken = (req, res, next) => {
+    console.log('inside verify token', req.headers.authorization);
+    // if (!req.headers.authorization) {
+    //   return res.status(401).send({ message: 'unauthorized access' });
+    // }
+    // const token = req.headers.authorization.split(' ')[1];
+    // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //   if (err) {
+    //     return res.status(401).send({ message: 'unauthorized access' })
+    //   }
+    //   req.decoded = decoded;
+    //   next();
+    // })
+    next();
+  }
+
+// user related api
+app.get('/users', verifyToken, async(req, res)=>{
+  console.log(req.headers);
   const result = await usercollection.find().toArray();
   res.send(result);
 })
