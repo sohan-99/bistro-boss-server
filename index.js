b4d6140d3a27d7fca61fa4f12dcad193a5376776
sohan-99 <sohan75632@gmail.com>
2023-11-22 13:01:21 +0600

    Send Price and Get client Secret from stripe server site

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middlewere 
@@ -205,6 +206,25 @@ app.post('/carts',async(req, res)=>{
   const result = await cartcollection.insertOne(cartItem);
   res.send(result);
 })
+
+ // payment intent
+ app.post('/create-payment-intent', async (req, res) => {
+  const { price } = req.body;
+  const amount = parseInt(price * 100);
+  console.log(amount, 'amount inside the intent')
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    payment_method_types: ['card']
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+});
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
